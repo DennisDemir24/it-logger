5d1b6b059d7c696fc4fa3717c7f6b8c6175a63a0
Dennis Demir <dd222gc@student.lnu.se>
2022-03-07 14:43:37 +0100

    initial commit

---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from 'react-redux'
+import { deleteTech } from '../../actions/techActions'
+
+const TechItem = ({ tech, deleteTech }) => {
+
+  return (
+    <li className="collection-item">
+      <div>
+        {tech.firstName} {tech.lastName}
+        <a onClick={() => deleteTech(tech.id)} href="#!" className="secondary-content">
+          <i className="metarial-icon grey-text">delete</i>
+        </a>
+      </div>
+    </li>
+  )
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
+};
+
+export default connect(null, { deleteTech })(TechItem);
